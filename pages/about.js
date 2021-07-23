0da6a097e94c52a27869b3292ffd4cfddc4552da
jalbarado <jalbarado741@gmail.com>
2021-07-23 14:37:05 -0500

    Link, enlazando páginas

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const About = () => {
   return (
     <div>
+      <Navbar/>
       <h1>Este es el About!!!</h1>
     </div>
   )
